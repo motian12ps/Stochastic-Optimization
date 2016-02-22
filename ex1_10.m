efc3859f3d1d059e091a5c8da5cb149630ba8d4a
motian12ps <chentianyi@ChenTianyis-MacBook-Pro-2.local>
2016-02-21 22:20:04 -0500

    exercise 1.10

--- ex1_10.m
+++ ex1_10.m
@@ -3,7 +3,7 @@ function ex1_10()
 theta0 = [0 , 3]';
 max_iter = 6;
 iter = 0;
-ak = [0.062,0.24,0.11,0.31,0.12,0.36];
+ak = [0.062, 0.24, 0.11, 0.31, 0.12, 0.36];
 theta = zeros(2,max_iter+1);
 fval = zeros(1, max_iter+1);
 while 1
@@ -13,22 +13,22 @@ while 1
     if iter == 0
         theta(:, iter + 1) = theta0;        
     else
-        theta(:, iter + 1) = theta(:, iter) - ak(iter)*grad(theta(:, iter));
+        theta(:, iter + 1) = theta(:, iter) - ak(iter) * grad(theta(:, iter) );
     end
     
-    fval(iter + 1) = fun(theta(:, iter + 1));
+    fval(iter + 1) = fun( theta(:, iter + 1) );
     iter = iter + 1;
 end
 theta
 fval
 end
 
-function [fvalue] = fun(theta)
-    fvalue = ( theta(1) - 2 )^4 + ( theta(1) - 2*theta(2) )^2; 
+function [fvalue] = fun( theta )
+    fvalue = ( theta(1) - 2 )^4 + ( theta(1) - 2 * theta(2) )^2;
 end
 
-function [g] = grad(theta)
-    g = zeros(size(theta));
-    g(1) = 4*(theta(1) - 2)^3 + 2*(theta(1) - 2*theta(2) );
-    g(2) = -4*(theta(1) - 2*theta(2) );
+function [g] = grad( theta )
+    g = zeros( size(theta) );
+    g(1) = 4 * ( theta(1) - 2 )^3 + 2 * ( theta(1) - 2 * theta(2) );
+    g(2) = -4 * ( theta(1) - 2 * theta(2) );
 end
